5dddc12b8b939d2d5d538a94722db4bcc7f6ca0e
Mike Q <mikeqiu54@gmail.com>
2025-06-26 14:53:16 -0700

    fixed more header file issues

--- SortCodeQYZ.h
+++ SortCodeQYZ.h
@@ -27,14 +27,14 @@
 #include "TEnv.h"
 using namespace std;
 
-TList *tigList, *emmaList, *s3List, *PIDList;
+TList *tigList, *emmaList, *s3List, *PIDList; *tofList; 
 
 // TIGRESS 
 TH1F *tigE, *tigAddE, *tigAddDoppE;
 
 // EMMA
-TH1F *emmaXpos *emmaYpos *emmaICSum, *emmaICE[5], *emmaSSBE[2], emmaSiE;
-TH2F *emmadEE, *emmaTigdt, *emmaPgac, *emmaICN, *emmaIC0V1, *emmaIC0V2, *emmaIC0V3, *emmaIC1V2*, *emmaIC1V3, *emmaIC2V3;
+TH1F *emmaXpos, *emmaYpos, *emmaICSum, *emmaICE[5], *emmaSSBE[2], emmaSiE;
+TH2F *emmadEE, *emmaTigdt, *emmaPgac, *emmaICN, *emmaIC0V1, *emmaIC0V2, *emmaIC0V3, *emmaIC1V2, *emmaIC1V3, *emmaIC2V3;
 TH2F *emmadtE, *emmaICSumVSi, *emmaICSumVSiPlusIC, *emmaIC0VSi, *emmaIC1VSi, *emmaIC2VSi, *emmaIC3VSi;
 
 // S3
@@ -47,7 +47,7 @@ TH1F *s3EmmaTof, *ssbICTof, *ssbSiTof, *tigICTof, *tigAnodeTof;
 // PID
 
 // Cuts 
-TCutG *24NeCut, *26MgCut, *26AlCut, *22NaCut; 
+TCutG *Ne24Cut, *Mg26Cut, *Al26Cut, *Na22Cut; 
 
 class SortCode {
 	public :
@@ -113,7 +113,6 @@ void SortCode::Initialise() {
 	s3RingsSectorsSinglesT = new TH1F("s3RingsSectorsSinglesT","S3 sectorT vs ringT Multiplicity 1;Sector Time - Ring Time",2048,-1024,1024);
 	s3RingsSectorsSinglesT->Add(s3RingsSectorsSinglesT); 
 	
-<<<<<<< HEAD
 	// TOF
 	TH1F *s3EmmaTof, *ssbICTof, *ssbSiTof, *tigICTof, *tigAnodeTof;
 	s3EmmaTof = new TH1F("s3EmmaTof", "S3 EMMA TOF; S3 EMMA Time Difference(ns);", 20000, -10000, 10000);
@@ -127,11 +126,6 @@ void SortCode::Initialise() {
 	tigAnodeTof = new TH1F("tigAnodeTof", "TIGRESS EMMA Anode TOF; TIGRESS EMMA Anode Time Difference(ns);", 20000, -10000, 10000);
 	tofList->Add(tigAnodeTof);
 	 
-
-=======
-	
->>>>>>> 7e8fe8d (fixed header file)
-
 
 	
 	

88b7bc86527e9cb9bbaefd9165071fe39a89813f
Mike Q <mikeqiu54@gmail.com>
2025-06-27 13:46:33 -0700

    fixed typos

--- SortCodeQYZ.cxx
+++ SortCodeQYZ.cxx
@@ -217,7 +217,7 @@ void SortCode::SortData(char const *afile, char const *calfile, char const *outf
         // EMMA gatede PGAC
         for (int i = 0; i < emma->GetMultiplicity(); i++){
             em_hit = emma->GetEmmaHit(i);
-            for (j = 0; j < s3->GetPixelMiltoplicity(); j++){
+            for (int j = 0; j < s3->GetPixelMultiplicity(); j++){
                 s3hit = s3->GetPixelHit(j);
                 if (s3hit->GetTime() - em_hit->GetTime() > 350 && s3hit->GetTime() - em_hit->GetTime() < 530) {
                     emmaPgac->Fill(em_hit->GetPosition().X(), em_hit->GetPosition().Y()); 

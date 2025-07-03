1ccb42aee0796171837b8031d16ac702343dbb42
Mike Q <mikeqiu54@gmail.com>
2025-07-03 14:37:19 -0700

    testing

--- SortCodeQYZ.cxx
+++ SortCodeQYZ.cxx
@@ -282,16 +282,13 @@ void SortCode::SortData(char const *afile, char const *calfile, char const *outf
                 si_hit = emma->GetSiHit(i);
                 if (Mg26_cut->IsInside(si_hit->GetEnergy(), tempIC) && s3)
                 {
-                    cout << "\nfirst if"<< endl; 
                     for (int j; j < s3->GetPixelMultiplicity(); j++)
                     {
                         s3hit = s3->GetPixelHit(j);
                         // if (s3hit->GetTime() - si_hit->GetTime() > s3_emma_T[0] && s3hit->GetTime() - si_hit->GetTime() < s3_emma_T[1] && tigress)
-                        if (tigress)
-                        {
-                            cout << "made it here!"<< endl;
                             for (int k; k < tigress->GetAddbackMultiplicity(); k++)
                             {
+                                count << "here"<< endl; 
                                 add_hit = tigress->GetAddbackHit(k);
                                 addDopp26MgPIDS3T->Fill(add_hit->GetDoppler(particle_betaDoppler)); 
                                  

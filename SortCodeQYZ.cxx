215c03ba154f04056e0707be9f082b5012f6e766
Mike Q <mikeqiu54@gmail.com>
2025-07-03 16:09:11 -0700

    reverted

--- SortCodeQYZ.cxx
+++ SortCodeQYZ.cxx
@@ -291,7 +291,7 @@ void SortCode::SortData(char const *afile, char const *calfile, char const *outf
                                 add_hit = tigress->GetAddbackHit(k);
                                 addDopp26MgPIDS3T->Fill(add_hit->GetDoppler(particle_betaDoppler)); 
                             }
-                        tigress->ResetAddback();
+                        //tigress->ResetAddback();
                     }
                 }
             }

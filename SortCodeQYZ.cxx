a097e3642e617cd4dbec8c3904e46b6d1f18bf98
Mike Q <mikeqiu54@gmail.com>
2025-07-04 14:07:26 -0700

    testing

--- SortCodeQYZ.cxx
+++ SortCodeQYZ.cxx
@@ -263,29 +263,29 @@ void SortCode::SortData(char const *afile, char const *calfile, char const *outf
 
 
             // EMMA - S3 Kinemtaics:
-            if (s3 && emma->GetMultiplicity() > 0)
-            {
-                for (int i = 0; emma->GetMultiplicity(); i++)
-                {
-                    em_hit = emma->GetEmmaHit(i);
-                    for (int j = 0; j < s3->GetPixelMultiplicity(); j++)
-                    {
-                        s3hit = s3->GetPixelHit(j);
-                        s3pos = s3hit->GetPosition(-101.25 * TMath::Pi() / 180., true); // rotation, s3 offset
-                        // s3pos.SetX(s3pos.X() + s3_x_offset);                            // these are all 0s, TODO: CONFIRM THIS
-                        // s3pos.SetY(s3pos.Y() + s3_y_offset);
-                        // s3pos.SetZ(s3pos.Z() + s3_z_offset);
-                        cout << "\nmade it just before s3-emma time gate" << endl; 
-                        if (s3hit->GetTime() - em_hit->GetTime() > s3_emma_T[0] && s3hit->GetTime() - em_hit->GetTime() < s3_emma_T[1])
-                        {
-                            thetalab = s3pos.Theta();                                                        // lab angle
-                            ekin = s3hit->GetEnergy();                                                       // triton energy
-                            exc = reac->GetExcEnergy(ekin * 1e-3, thetalab, 2);                              // 26Mg Excitation energy, Energy conversion from keV to MeV (1e-3), two-body reaction
-                            mg26ExcEmmaS3->Fill(exc);
-                        }
-                    }
-                }
-            }
+            // if (s3 && emma->GetMultiplicity() > 0)
+            // {
+            //     for (int i = 0; emma->GetMultiplicity(); i++)
+            //     {
+            //         em_hit = emma->GetEmmaHit(i);
+            //         for (int j = 0; j < s3->GetPixelMultiplicity(); j++)
+            //         {
+            //             s3hit = s3->GetPixelHit(j);
+            //             s3pos = s3hit->GetPosition(-101.25 * TMath::Pi() / 180., true); // rotation, s3 offset
+            //             // s3pos.SetX(s3pos.X() + s3_x_offset);                            // these are all 0s, TODO: CONFIRM THIS
+            //             // s3pos.SetY(s3pos.Y() + s3_y_offset);
+            //             // s3pos.SetZ(s3pos.Z() + s3_z_offset);
+            //             cout << "\nmade it just before s3-emma time gate" << endl; 
+            //             if (s3hit->GetTime() - em_hit->GetTime() > s3_emma_T[0] && s3hit->GetTime() - em_hit->GetTime() < s3_emma_T[1])
+            //             {
+            //                 thetalab = s3pos.Theta();                                                        // lab angle
+            //                 ekin = s3hit->GetEnergy();                                                       // triton energy
+            //                 exc = reac->GetExcEnergy(ekin * 1e-3, thetalab, 2);                              // 26Mg Excitation energy, Energy conversion from keV to MeV (1e-3), two-body reaction
+            //                 mg26ExcEmmaS3->Fill(exc);
+            //             }
+            //         }
+            //     }
+            // }
 
             // ----------------------------------- IC -----------------------------------
             for (int i = 0; i < emma->GetICMultiplicity(); i++)

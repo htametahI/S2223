ed264194a4613deb99a7845d7050030d281b673e
Mike Q <mikeqiu54@gmail.com>
2025-07-31 16:17:59 -0700

    testing

--- simpleSort.C
+++ simpleSort.C
@@ -1,12 +1,10 @@
 {
 
 	TChain *oak = new TChain("AnalysisTree");
-	oak->Add("analysistreeXXXXXXXX.root"); //change me!!
+	oak->Add("~/S2223/AnalysisTrees/UsingCalibrationFilePostExperiment/analysis61000_000.root"); //change me!!
 	TS3 *s3 = 0;
 	oak->SetBranchAddress("TS3",&s3);
-	TChannel::ReadCalFile("CalibrationFile.cal"); //change me!!
-
-
+	TChannel::ReadCalFile("CalibrationFilePostExperiment.cal"); //change me!!
 
 
 	TH1F *hist = new TH1F("hist","hist;Energy (keV);Counts",16384,0,16384);
@@ -14,18 +12,16 @@
 	for (int i = 0; i < oak->GetEntries(); i++){
 		oak->GetEntry(i);
 
-		for (int j = 0; j < s3->GetMultiplicity(); j++){
-			auto *s3_hit = s3->GetS3Hit(j);
-
+		for (int j = 0; j < s3->GetPixelMultiplicity(); j++){
+			auto *s3_hit = s3->GetPixelHit(j);
 			hist->Fill(s3_hit->GetEnergy());
-
 		}
 
 
 	}
 
 
-	TFile *outFile = new TFile("outfile.root","recreate"); //change me!!
+	TFile *outFile = new TFile("test.root","recreate"); //change me!!
 	hist->Write();
 	outFile->Close();
 

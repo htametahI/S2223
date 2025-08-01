616f93141198a68b2e6fc41c7fe258c72fb7a48c
Mike Q <mikeqiu54@gmail.com>
2025-08-01 15:33:08 -0700

    test

--- simpleSort.C
+++ simpleSort.C
@@ -1,29 +1,9 @@
-{
-#include <iostream>
-#include <iomanip>
-#include "TCutG.h"
-#include "TH1.h"
-#include "TF1.h"
-#include "TGraph.h"
-#include "TGraphErrors.h"
-#include "TTree.h"
-#include "TChain.h"
-#include "TH2.h"
-#include "TFile.h"
-#include "TDirectory.h"
-#include "TList.h"
-#include "TRandom.h"
-#include "TS3.h"
-#include "TS3Hit.h"
-#include "TReaction.h"
-#include "TSRIM.h"
-#include "TTigress.h"
-#include "TSpectrum.h"
-#include "TChannel.h"
-#include "TEmma.h"
-#include "TParserLibrary.h"
-#include "TEnv.h"
-using namespace std;
+	#include "TEmma.h"
+	#include "TReaction.h"
+	
+	{	
+
+
 
 	double ekin; 
 	TVector3 *s3pos; 

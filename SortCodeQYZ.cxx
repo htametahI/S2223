17bb6742aea0cbde98e1258368c462f705452303
Mike Q <mikeqiu54@gmail.com>
2025-09-15 16:07:52 -0700

    test

--- SortCodeQYZ.cxx
+++ SortCodeQYZ.cxx
@@ -265,7 +265,7 @@ void SortCode::SortData(char const *afile, char const *calfile, char const *outf
                         
                         if (tigress) {
                             for (int k = 0; k < tigress->GetAddbackMultiplicity(); k++) {
-                                // add_hit = tigress->GetAddbackHit(k);
+                                add_hit = tigress->GetAddbackHit(k);
                                 // gammaExc->Fill(add_hit->GetDoppler(particle_beta)); 
                                 // if (exc > 10.925 && exc < 10.975) gammaExc10p9->Fill(add_hit->GetDoppler(particle_beta));
                                 // if (exc > 11.145 && exc < 11.195) gammaExc11p09->Fill(add_hit->GetDoppler(particle_beta));

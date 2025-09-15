375fc28ae27dca9df8994c4cbc944e2bab3187e1
Mike Q <mikeqiu54@gmail.com>
2025-09-15 16:21:44 -0700

    adjusted gates

--- SortCodeQYZ.cxx
+++ SortCodeQYZ.cxx
@@ -267,8 +267,8 @@ void SortCode::SortData(char const *afile, char const *calfile, char const *outf
                             for (int k = 0; k < tigress->GetAddbackMultiplicity(); k++) {
                                 add_hit = tigress->GetAddbackHit(k);
                                 gammaExc->Fill(add_hit->GetDoppler(particle_beta)); 
-                                if (exc > 10.925 && exc < 10.975) gammaExc10p9->Fill(add_hit->GetDoppler(particle_beta));
-                                if (exc > 11.145 && exc < 11.195) gammaExc11p09->Fill(add_hit->GetDoppler(particle_beta));
+                                if (exc > 10.62 && exc < 11.09) gammaExc10p9->Fill(add_hit->GetDoppler(particle_beta));
+                                if (exc > 11.09 && exc < 11.50) gammaExc11p09->Fill(add_hit->GetDoppler(particle_beta));
                             }
                         }
 

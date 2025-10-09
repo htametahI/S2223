663e3bf75f98b321952216f2751d330b71689363
Mike Q <mikeqiu54@gmail.com>
2025-10-08 22:55:14 -0700

    testing

--- SortCodeQYZ.cxx
+++ SortCodeQYZ.cxx
@@ -135,7 +135,7 @@ void SortCode::SortData(char const *afile, char const *calfile, char const *outf
     double EBeam = 64.14166; // Beam energy half way into the target
 
     //TReaction *reac = new TReaction("22Ne", "7Li", "3H", "26Mg", 64.14166, 0, true);
-    TReaction *reac = new TReaction("7Li", "22Ne", "22Ne", "3H", 64.14166, 0, true);
+    TReaction *reac = new TReaction("4He", "4He", "7Li", "H", 64.14166, 0, true);
 
     // Defining Pointers
     TEmmaHit *em_hit, *ic_hit, *si_hit, *ssb_hit, *trigger_hit, *anode_hit;
